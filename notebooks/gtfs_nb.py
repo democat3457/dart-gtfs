4f5633f1cbdb30e95536a52e1e6a77fbfb6d0e5c
democat <cwongmath@gmail.com>
2025-01-11 15:52:51 -0600

    route and stop timetable experiments

--- notebooks/gtfs_nb.py
+++ notebooks/gtfs_nb.py
@@ -9,29 +9,72 @@
 #       format_version: '1.3'
 #       jupytext_version: 1.11.2
 #   kernelspec:
-#     display_name: Python 3
+#     display_name: 3.10.13
 #     language: python
 #     name: python3
 # ---
 
 # %% [markdown]
-# DART GTFS data: https://www.dart.org/about/about-dart/fixed-route-schedule
+# DART GTFS data: https://www.dart.org/about/about-dart/fixed-route-schedule  
 # https://www.dart.org/transitdata/latest/google_transit.zip
 
 # %%
 from pathlib import Path
 import gtfs_kit as gk
+import folium
+
+data_folder = Path("../data")
+if not data_folder.exists():
+    data_folder.mkdir()
+export_folder = Path("../export")
+if not export_folder.exists():
+    export_folder.mkdir()
+file = Path(data_folder) / "google_transit.zip"
 
 # %%
-file = Path("google_transit.zip")
+import requests
+import shutil
 
+r = requests.get("https://www.dart.org/transitdata/latest/google_transit.zip", stream=True)
+if r.status_code == 200:
+    with file.open("wb") as out_file:
+        r.raw.decode_content = True
+        shutil.copyfileobj(r.raw, out_file)
+
+# %%
+Path().cwd()
+
+# %%
 feed = gk.read_feed(file, dist_units="mi")
 feed.describe()
 
 # %%
 routes = feed.routes
 for x in routes[routes["route_long_name"].str.contains("ROSS")]["route_id"]:
-    print(type(x))
+    print(type(x), x)
+
+# %%
+feed.stops
+
+# %%
+from pykml import parser
+with (data_folder / "Boundary.kml").open() as f:
+    doc = parser.parse(f)
+
+boundary_coords = [ tuple(map(float, line.strip().split(',')))[1::-1] for line in doc.findall(".//{http://www.opengis.net/kml/2.2}Placemark")[0].find(
+    ".//{http://www.opengis.net/kml/2.2}coordinates"
+).text.strip().splitlines() ]
+
+# %%
+m = folium.Map(location=[32.7769, -96.7972], zoom_start=10)
+
+for idx, stop in feed.stops.iterrows():
+    name, lat, lon = stop["stop_desc"], stop["stop_lat"], stop["stop_lon"]
+    folium.Circle(location=[lat, lon], popup=name, fill_color="#f00", fill_opacity=1, weight=0, radius=804.672).add_to(m)
+
+folium.Polygon(locations=boundary_coords, color="black", fill=False).add_to(m)
+
+m
 
 # %%
 # Choose study dates
@@ -48,5 +91,46 @@ feed.build_route_timetable(route_id, dates).T
 # %%
 rids = feed.routes.route_id.loc[:]
 print(feed.routes.route_id.loc[0])
-feed.routes.to_csv("routes.csv")
+feed.routes.to_csv(export_folder / "routes.csv")
+feed.stops.to_csv(export_folder / "stops.csv")
 # feed.map_routes(rids, show_stops=False)
+
+# %%
+trip_stats = feed.compute_trip_stats()
+feed.compute_route_stats(trip_stats, ["20241216"]).to_csv(export_folder / "route_stats_20241216.csv")
+
+# %%
+feed.compute_route_time_series(trip_stats, ["20241216"]).to_csv(export_folder / "route_time_series_20241216.csv")
+
+# %%
+feed.build_stop_timetable("22750", ["20241216"])
+
+# %%
+dtstf = feed.append_dist_to_stop_times()
+
+# %%
+print(dtstf)
+
+# %%
+feed.stop_times
+
+# %%
+feed.stop_times[feed.stop_times["trip_id"]=="8107832"]
+
+# %%
+feed.stop_times[feed.stop_times["timepoint"]==1]
+
+# %%
+feed.trips[feed.trips["route_id"] == "25753"]
+
+# %%
+feed.shapes
+
+# %%
+feed.calendar_dates
+
+# %%
+feed.build_route_timetable("25753", ["20241219"])
+
+# %%
+feed.feed_info.to_json()
